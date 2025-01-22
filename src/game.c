66bfc7a9a0ddc564a85d464fbd91ca16ce37e9be
Izabel Zanoni <izanoni@c3r5p1.42sp.org.br>
2025-01-22 15:51:55 -0300

    .

---
+++ src/game.c
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: izanoni <izanoni@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 15:46:07 by izanoni           #+#    #+#             */
+/*   Updated: 2025/01/22 15:46:54 by izanoni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "MLX42/MLX42.h"
+#define WIDTH 512	
+#define HEIGHT 512
+
+static void error(void)
+{
+	puts(mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
+
+int32_t	main(void)
+{
+	// Start mlx
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Cub3D", true);
+	if (!mlx)
+        error();
+
+	// Try to load the file
+	mlx_texture_t* texture = mlx_load_png("texture/wall_north.png");
+	if (!texture)
+        error();
+	
+	// Convert texture to a displayable image
+	mlx_image_t* img = mlx_texture_to_image(mlx, texture);
+	if (!img)
+        error();
+
+	// Display the image
+	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
+        error();
+
+	mlx_loop(mlx);
+
+	// Optional, terminate will clean up any leftovers, this is just to demonstrate.
+	mlx_delete_image(mlx, img);
+	mlx_delete_texture(texture);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
+
+/* #include "game.h"
+
+
+
+// Bytes Per Pixel. Since each pixel is represented as an integer, it will be four bytes for four channels.
+#define BPP sizeof(int32_t)
+
+int32_t	main(void)
+{
+    // Init mlx with a canvas size of 256x256 and the ability to resize the window.
+    mlx_t* mlx = mlx_init(256, 256, "MLX42", true);
+    
+    if (!mlx) exit(EXIT_FAILURE);
+
+    // Create a 128x128 image.
+    mlx_image_t* img = mlx_new_image(mlx, 128, 128);
+
+    // Set the channels of each pixel in our image to the maximum byte value of 255. 
+    memset(img->pixels, 255, img->width * img->height * BPP);
+
+    // Draw the image at coordinate (0, 0).
+    mlx_image_to_window(mlx, img, 0, 0);
+
+    // Run the main loop and terminate on quit.  
+    mlx_loop(mlx);
+    mlx_terminate(mlx);
+
+    return (EXIT_SUCCESS);
+} */
+
+/* 
+// Exit the program as failure.
+static void ft_error(void)
+{
+	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
+
+// Print the window width and height.
+static void ft_hook(void* param)
+{
+	const mlx_t* mlx = param;
+
+	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
+}
+
+int32_t	main(void)
+{
+
+	// MLX allows you to define its core behaviour before startup.
+	mlx_set_setting(MLX_MAXIMIZED, true);
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
+	if (!mlx)
+		ft_error();
+
+	// Do stuff
+
+	// Create and display the image.
+	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
+	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+		ft_error();
+
+	// Even after the image is being displayed, we can still modify the buffer.
+	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
+
+	// Register a hook and pass mlx as an optional param.
+	// NOTE: Do this before calling mlx_loop!
+	mlx_loop_hook(mlx, ft_hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
+ */
