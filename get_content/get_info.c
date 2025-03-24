1d6ba6d09d2ef7c604324401468f49bb9fbd290c
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2025-03-24 18:13:42 -0300

    fixed norm

--- get_content/get_info.c
+++ get_content/get_info.c
@@ -12,87 +12,88 @@
 
 #include "../cub_3d.h"
 
-int get_texture_path(char **matrix, t_content *s_content)
+int	get_texture_path(char **matrix, t_content *s_content)
 {
-	int cols;
-	int count;
+	int	cols;
+	int	count;
 
 	cols = 0;
 	count = 0;
-
-		while(matrix[cols] != NULL)
-		{
-				count = walk_spaces(matrix[cols]);	
-				if(matrix[cols][count] == 'N')
-					s_content->NO_path = get_string(matrix[cols], count + 2);
-				else if(matrix[cols][count] == 'S')
-				s_content->SO_path = get_string(matrix[cols], count + 2);
-				else if(matrix[cols][count] == 'E')
-					s_content->EA_path = get_string(matrix[cols], count + 2);
-				else if(matrix[cols][count] == 'W')
-					s_content->WE_path = get_string(matrix[cols], count + 2);
-			cols++;	
-		}
-	if(!s_content->EA_path || !s_content->NO_path || !s_content->SO_path || !s_content->WE_path)
-		return(0);
-	return(1);
+	while (matrix[cols] != NULL)
+	{
+		count = walk_spaces(matrix[cols]);
+		if (matrix[cols][count] == 'N')
+			s_content->NO_path = get_string(matrix[cols], count + 2);
+		else if (matrix[cols][count] == 'S')
+			s_content->SO_path = get_string(matrix[cols], count + 2);
+		else if (matrix[cols][count] == 'E')
+			s_content->EA_path = get_string(matrix[cols], count + 2);
+		else if (matrix[cols][count] == 'W')
+			s_content->WE_path = get_string(matrix[cols], count + 2);
+		cols++;
+	}
+	if (!s_content->EA_path || !s_content->NO_path || !s_content->SO_path
+		|| !s_content->WE_path)
+		return (0);
+	return (1);
 }
 
-char *get_string(char *string, int count)
+char	*get_string(char *string, int count)
 {
-	char *result;
-	int i;
+	char	*result;
+	int		i;
 
 	i = 0;
 	result = ft_calloc((ft_strlen(string)), sizeof(char));
 	while (string[count] == ' ' || string[count] == '\t')
-			count++;
+		count++;
 	while (string[count] != '\0' && string[count] != '\n')
 	{
-		result[i] =  string[count];
+		result[i] = string[count];
 		i++;
 		count++;
 	}
-	return(result);
+	return (result);
 }
 
-int get_color(char **matrix, t_content *s_content)
+int	get_color(char **matrix, t_content *s_content)
 {
-	int cols;
-	int count;
-	int i;
-	char *temp;
-	
+	int		cols;
+	int		count;
+	int		i;
+	char	*temp;
+
 	i = 0;
 	count = 0;
 	cols = 0;
 	temp = NULL;
-	while(matrix[cols] != NULL)
+	while (matrix[cols] != NULL)
 	{
-		count = walk_spaces(matrix[cols]);	
+		count = walk_spaces(matrix[cols]);
 		i = 0;
-		if(matrix[cols][count] == 'F' || matrix[cols][count] == 'C')
+		if (matrix[cols][count] == 'F' || matrix[cols][count] == 'C')
 		{
 			temp = ft_calloc(ft_strlen(matrix[cols]), sizeof(char *));
-			while (matrix[cols][count] && (matrix[cols][count] < '0' || matrix[cols][count] > '9'))
+			while (matrix[cols][count] && (matrix[cols][count] < '0'
+					|| matrix[cols][count] > '9'))
 				count++;
 			while (matrix[cols][count] != '\0' && matrix[cols][count] != '\n')
 			{
-				temp[i] =  matrix[cols][count];
+				temp[i] = matrix[cols][count];
 				i++;
 				count++;
 			}
-			if(ft_strchr(matrix[cols], 'F') != NULL)
+			if (ft_strchr(matrix[cols], 'F') != NULL)
 				s_content->color_F = ft_split(temp, ',');
-			else if(ft_strchr(matrix[cols], 'C') != NULL)
+			else if (ft_strchr(matrix[cols], 'C') != NULL)
 				s_content->color_C = ft_split(temp, ',');
 			free(temp);
-			}
-			cols++;	
+		}
+		cols++;
 	}
-	if(!s_content->color_C || !s_content->color_F)
-		return(print_error(ERROR_3));
-	return(1);
+	if (!s_content->color_C || !s_content->color_F)
+		return (print_error(ERROR_3));
+	return (1);
 }
 
 uint32_t	ft_pixel(uint32_t r, uint32_t g, uint32_t b, uint32_t a)
@@ -100,13 +101,11 @@ uint32_t	ft_pixel(uint32_t r, uint32_t g, uint32_t b, uint32_t a)
 	return (r << 24 | g << 16 | b << 8 | a);
 }
 
-void 	get_color_number(t_content *s_content)
+void	get_color_number(t_content *s_content)
 {
-	
-	int number[3];
-	int number2[3];
-	
-	
+	int	number[3];
+	int	number2[3];
+
 	number[0] = ft_atoi(s_content->color_F[0]);
 	number[1] = ft_atoi(s_content->color_F[1]);
 	number[2] = ft_atoi(s_content->color_F[2]);
@@ -117,90 +116,91 @@ void 	get_color_number(t_content *s_content)
 	s_content->ceiling = ft_pixel(number2[0], number2[1], number2[2], 255);
 }
 
-int get_map(t_args *s_map, t_content *s_content)
+int	get_map(t_args *s_map, t_content *s_content)
 {
-	int i;
-	int j;
-	int lines_map;
-	
+	int	i;
+	int	j;
+	int	lines_map;
+
 	j = 0;
 	i = 0;
 	lines_map = 0;
 	lines_map = s_map->map_position;
 	s_content->map = malloc((lines_map + 1) * sizeof(char *));
-	if(!s_content->map)
-		return(print_error(ERROR_3));
+	if (!s_content->map)
+		return (print_error(ERROR_3));
 	while (s_map->matrix[lines_map] != NULL)
 	{
 		i = ft_strlen(s_map->matrix[lines_map]);
 		s_content->map[j] = malloc((i + 1) * sizeof(char));
-		if(!s_content->map[j])
+		if (!s_content->map[j])
 		{
 			free_matrix(s_content->map);
-			return(print_error(ERROR_3));
+			return (print_error(ERROR_3));
 		}
 		ft_strlcpy(s_content->map[j], s_map->matrix[lines_map], i);
 		j++;
 		lines_map++;
 	}
 	s_content->map[j] = NULL;
-	return(1);
+	return (1);
 }
 
 int	get_map_sizes_y(t_content *s_content)
 {
 	int	i;
-	int count;
+	int	count;
+
 	i = 0;
 	count = 0;
-	while(s_content->map[i] != NULL)
+	while (s_content->map[i] != NULL)
 	{
 		while (s_content->map[i][count] == ' ')
 			count++;
-		if(s_content->map[i][count] == '\n' || s_content->map[i][count] == '\0')
-			break;
+		if (s_content->map[i][count] == '\n'
+			|| s_content->map[i][count] == '\0')
+			break ;
 		i++;
 	}
-	return(i);
+	return (i);
 }
 
-int get_map_sizes_x(t_content *s_content)
+int	get_map_sizes_x(t_content *s_content)
 {
-	int i;
-	int size_line;
-	int comp_size;
-	
+	int	i;
+	int	size_line;
+	int	comp_size;
+
 	i = 0;
 	size_line = 0;
 	comp_size = 0;
-
 	size_line = ft_strlen(s_content->map[i]);
-	while(s_content->map[i] != NULL)
+	while (s_content->map[i] != NULL)
 	{
 		comp_size = ft_strlen(s_content->map[i]);
-		if(comp_size > size_line)
+		if (comp_size > size_line)
 			size_line = comp_size;
 		i++;
 	}
-	return(size_line);
+	return (size_line);
 }
 
-int get_info(t_args *s_map, t_content *s_content)
+int	get_info(t_args *s_map, t_content *s_content)
 {
-	if(!get_map(s_map, s_content))
+	if (!get_map(s_map, s_content))
 	{
 		printf("error when creating map\n");
-		return(0);
+		return (0);
 	}
-	if(!get_color(s_map->matrix, s_content))
+	if (!get_color(s_map->matrix, s_content))
 	{
 		printf("error when creating color\n");
-		return(0);
+		return (0);
 	}
-	if(!get_texture_path(s_map->matrix, s_content))
+	if (!get_texture_path(s_map->matrix, s_content))
 	{
 		printf("error when creating texture\n");
-		return(0);
+		return (0);
 	}
 	get_color_number(s_content);
 	return (1);
